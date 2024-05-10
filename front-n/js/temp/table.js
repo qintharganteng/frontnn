87e7ac3fbdb214233e31e0f9683a39bbd78014eb
qintharganteng <qintharalmaliki@gmail.com>
2024-05-11 00:46:31 +0700

    last

--- front-n/js/temp/table.js
+++ front-n/js/temp/table.js
@@ -2,19 +2,19 @@ export let isiTabel =
 `
 <tr>
             <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                          
+                          #ID#
                         </th>
           <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                          
+                          #Nama#
                         </th>
            <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                          
+                          #alamat#
                         </th>
           <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                          
+                          #no_telp
                         </th>
 		<th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-							
+							#membership_id#
 						  </th>
           </tr>
-                        `
+ `
